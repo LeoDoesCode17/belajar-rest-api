e833ab938e6ba08a5d3b6510c179e9ddbc91215e
Leo <leonardonifinluri@gmail.com>
2025-05-20 15:20:23 +0800

    add address model and migration

---
+++ app/Models/Address.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'addresses';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $timestamps = true;
+    public $incrementing = true;
+}
