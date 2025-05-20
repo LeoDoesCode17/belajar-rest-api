5a5b6597bc5ab3cc9b0ba8f209d8bd7dd29eb517
Leo <leonardonifinluri@gmail.com>
2025-05-20 15:07:01 +0800

    feat: add contact model and migration

---
+++ app/Models/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    protected $table = 'contacts';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $timestamps = true;
+    public $incrementing = true;
+}
