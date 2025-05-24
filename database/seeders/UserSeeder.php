45022949630884b0b959e1aaa1a9397195197c63
Leo <leonardonifinluri@gmail.com>
2025-05-24 16:29:40 +0800

    feat: add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'username' => 'creator09',
+            'password' => Hash::make('12345678'),
+            'name' => 'Leonardo'
+        ]);
     }
 }
