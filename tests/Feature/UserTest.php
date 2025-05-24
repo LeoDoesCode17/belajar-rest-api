90bee53a42cfad5d10c1f842c12c06e6c8977f5f
Leo <leonardonifinluri@gmail.com>
2025-05-24 16:32:20 +0800

    feat: add test case for user login success and fail

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Illuminate\Support\Facades\Hash;
+
+use function PHPUnit\Framework\assertNotNull;
 
 uses(RefreshDatabase::class);
 test('User register successfully.', function () {
@@ -34,7 +37,7 @@ test('Register failed due to all fields are empty.', function () {
         ]);
 });
 
-test('Register failed due to one field is empty', function () {
+test('Register failed due to one field is empty.', function () {
     $this->post('/api/users', [
         'username' => '',
         'password' => '12345678',
@@ -47,7 +50,7 @@ test('Register failed due to one field is empty', function () {
         ]);
 });
 
-test('Register failed due to existed username', function () {
+test('Register failed due to existed username.', function () {
     // create a user
     $username = 'Creator09';
     $user = new User();
@@ -60,10 +63,36 @@ test('Register failed due to existed username', function () {
         'username' => $username,
         'password' => '12345678',
         'name' => 'Leonardo'
-    ])->assertStatus(400)
-        ->assertJson([
-            'errors' => [
-                'username' => ['The username has already been taken.'],
-            ]
-        ]);
+    ])->assertStatus(400)->assertJson([
+        'errors' => [
+            'username' => ['The username has already been taken.'],
+        ]
+    ]);
+});
+
+test('User login successfully.', function () {
+    $this->seed([UserSeeder::class]);
+    $this->post('/api/users/login', [
+        'username' => 'creator09',
+        'password' => '12345678'
+    ])->assertStatus(200)->assertJson([
+        'data' => [
+            'username' => 'creator09',
+            'name' => 'Leonardo'
+        ]
+    ]);
+    $user = User::where('username', 'creator09')->first();
+    $this->assertNotNull($user->token);
+});
+
+test('User login failed.', function () {
+    $this->seed([UserSeeder::class]);
+    $this->post('/api/users/login', [
+        'username' => 'creator09',
+        'password' => '123456789'
+    ])->assertStatus(401)->assertJson([
+        'errors' => [
+            'message' => ['Wrong credentials']
+        ]
+    ]);
 });
