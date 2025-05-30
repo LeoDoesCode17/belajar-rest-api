28c40759e2f815404832a00a5167ae5f280a6907
Leo <leonardonifinluri@gmail.com>
2025-05-30 12:19:36 +0800

    feat: add new Contact controller and add create contact function

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Contact\ContactCreateRequest;
+use App\Http\Resources\ContactResource;
+use Illuminate\Http\JsonResponse;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function create(ContactCreateRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $contact = new Contact($data);
+        $contact->save();
+        return (new ContactResource($contact))->response()->setStatusCode(201);
+    }
+}
