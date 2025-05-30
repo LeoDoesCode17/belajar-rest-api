2cf2c6ef5a56ead14ed96bd55c81ac1fad988946
Leo <leonardonifinluri@gmail.com>
2025-05-30 15:53:21 +0800

    feat: add test cases to test get contact api by id success and failed

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -8,8 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use function PHPUnit\Framework\assertNotEquals;
 
 uses(RefreshDatabase::class);
-test('Create contact success.', function () {
-    $this->seed(UserSeeder::class); // imitate logged in user
+test('Succeed create a new contact.', function () {
+    $this->seed([UserSeeder::class]); // imitate logged in user
     $user = User::where('username', 'creator09')->first();
     $this->withHeaders([
         'Authorization' => $user->token
@@ -30,8 +30,8 @@ test('Create contact success.', function () {
     ]);
 });
 
-test('Fail to create new contact due to missing/invalid token.', function () {
-    $this->seed(UserSeeder::class);
+test('Failed to create new contact due to missing/invalid token.', function () {
+    $this->seed([UserSeeder::class]);
     $user = User::where('username', 'creator09')->first();
     $user->token = null;
     $user->save();
@@ -50,8 +50,8 @@ test('Fail to create new contact due to missing/invalid token.', function () {
     ]);
 });
 
-test('Fail to create contact due to one field exceed limit length.', function () {
-    $this->seed(UserSeeder::class); // imitate logged in user
+test('Failed to create contact due to one field exceed limit length.', function () {
+    $this->seed([UserSeeder::class]); // imitate logged in user
     $user = User::where('username', 'creator09')->first();
     $this->withHeaders([
         'Authorization' => $user->token
@@ -68,8 +68,8 @@ test('Fail to create contact due to one field exceed limit length.', function ()
     ]);
 });
 
-test('Success update a contact.', function () {
-    $this->seed(UserSeeder::class);
+test('Succeed update a contact.', function () {
+    $this->seed([UserSeeder::class]);
     $user = User::where('username', 'creator09')->first();
     $contact = Contact::create([
         'user_id' => $user->id,
@@ -97,8 +97,8 @@ test('Success update a contact.', function () {
     assertNotEquals($oldPhone, $newPhone);
 });
 
-test('Fail update a contact due to missing/invalid token.', function () {
-    $this->seed(UserSeeder::class);
+test('Failed update a contact due to missing/invalid token.', function () {
+    $this->seed([UserSeeder::class]);
     $user = User::where('username', 'creator09')->first();
     $user->token = null;
     $user->save();
@@ -121,8 +121,8 @@ test('Fail update a contact due to missing/invalid token.', function () {
     ]);
 });
 
-test('Fail update a contact due to one field exceed limit length.', function () {
-    $this->seed(UserSeeder::class);
+test('Failed update a contact due to one field exceed limit length.', function () {
+    $this->seed([UserSeeder::class]);
     $user = User::where('username', 'creator09')->first();
     $contact = Contact::create([
         'user_id' => $user->id,
@@ -141,4 +141,58 @@ test('Fail update a contact due to one field exceed limit length.', function () 
             'phone' => ['The phone field must not be greater than 20 characters.']
         ]
     ]);
+});
+
+test('Succeed to get a contact by id.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = Contact::create([
+        'user_id' => $user->id,
+        'first_name' => 'Leonardo',
+        'last_name' => 'Nifinluri',
+        'phone' => '082188889999',
+        'email' => 'leonardo@gmail.com'
+    ]);
+    $contactId = $contact->id;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->get("/api/contacts/$contactId")->assertStatus(200)->assertJson([
+        'data' => [
+            'user_id' => $user->id,
+            'first_name' => 'Leonardo',
+            'last_name' => 'Nifinluri',
+            'phone' => '082188889999',
+            'email' => 'leonardo@gmail.com'
+        ]
+    ]);
+});
+
+test('Failed to get a contact by id due to missing/invalid token.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $user->token = null;
+    $user->save();
+    $contact = Contact::create([
+        'user_id' => $user->id,
+        'first_name' => 'Leonardo',
+        'last_name' => 'Nifinluri',
+        'phone' => '082188889999',
+        'email' => 'leonardo@gmail.com'
+    ]);
+    $contactId = $contact->id;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->get("/api/contacts/$contactId")->assertStatus(401)->assertJson([
+        'errors' => [
+            'message' => ['unauthorized']
+        ]
+    ]);
+});
+
+test('Failed to get a contact by id due to not found contact.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->get("/api/contacts/8")->assertStatus(404);
 });
