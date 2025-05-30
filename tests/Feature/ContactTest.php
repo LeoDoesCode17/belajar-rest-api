591e3b257e5509ea7cf486fa6f47577f8157895b
Leo <leonardonifinluri@gmail.com>
2025-05-30 16:08:07 +0800

    feat: add test cases to delete a contact success and failed

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -196,3 +196,51 @@ test('Failed to get a contact by id due to not found contact.', function () {
         'Authorization' => $user->token
     ])->get("/api/contacts/8")->assertStatus(404);
 });
+
+test('Succeed to delete a contact.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = Contact::create([
+        'user_id' => $user->id,
+        'first_name' => 'Leonardo',
+        'last_name' => 'Nifinluri',
+        'phone' => '082188889999',
+        'email' => 'leonardo@gmail.com'
+    ]);
+    $contactId = $contact->id;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete("/api/contacts/$contactId")->assertStatus(200)->assertJson([
+        'data' => true
+    ]);
+});
+
+test('Failed to delete a contact due to missing/invalid token.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $user->token = null;
+    $user->save();
+    $contact = Contact::create([
+        'user_id' => $user->id,
+        'first_name' => 'Leonardo',
+        'last_name' => 'Nifinluri',
+        'phone' => '082188889999',
+        'email' => 'leonardo@gmail.com'
+    ]);
+    $contactId = $contact->id;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete("/api/contacts/$contactId")->assertStatus(401)->assertJson([
+        'errors' => [
+            'message' => ['unauthorized']
+        ]
+    ]);
+});
+
+test('Failed to delete a contact due to not found contact.', function () {
+    $this->seed([UserSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete("/api/contacts/9")->assertStatus(404);
+});
