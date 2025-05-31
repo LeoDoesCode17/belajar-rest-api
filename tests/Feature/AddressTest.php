325dce2081be4c3629d7100b61930459e6f48ff6
Leo <leonardonifinluri@gmail.com>
2025-05-31 14:19:50 +0800

    feat: add test cases to delete an address failed and success

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -8,6 +8,8 @@ use Database\Seeders\AddressSeeder;
 
 use function PHPUnit\Framework\assertNotEmpty;
 use function PHPUnit\Framework\assertNotEquals;
+use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertNull;
 
 uses(RefreshDatabase::class);
 
@@ -224,4 +226,69 @@ test('Failed to update an address due to one column value exceeds length limit.'
             'postal_code' => ['The postal code field must not be greater than 10 characters.']
         ]
     ]);
+});
+
+test('Succeed delete an address.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete('/api/contacts/' . $contact->id . '/addresses/' . $address->id)->assertStatus(200)->assertJson([
+        'data' => true
+    ]);
+    $deletedAddress = App\Models\Address::where('id', $address->id)->first();
+    assertNull($deletedAddress);
+});
+
+test('Failed to delete an address of other user.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user1 = User::where('username', 'creator09')->first();
+    $user2 = User::where('username', 'bowo09')->first();
+    $contact = $user1->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user2->token
+    ])->delete('/api/contacts/' . $contact->id . '/addresses/' . $address->id)->assertStatus(404)->assertJson([
+        'errors' => [
+            'message' => ['not found']
+        ]
+    ]);
+    $deletedAddress = App\Models\Address::where('id', $address->id)->first();
+    assertNotNull($deletedAddress);
+});
+
+test('Failed to delete an address due to invalid/missing token.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $user->token = null;
+    $user->save();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete('/api/contacts/' . $contact->id . '/addresses/' . $address->id)->assertStatus(401)->assertJson([
+        'errors' => [
+            'message' => ['unauthorized']
+        ]
+    ]);
+    $deletedAddress = App\Models\Address::where('id', $address->id)->first();
+    assertNotNull($deletedAddress);
+});
+
+test('Failed to delete an address due to not found address.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->delete('/api/contacts/' . $contact->id . '/addresses/' . ($address->id + 100))->assertStatus(404)->assertJson([
+        'errors' => [
+            'message' => ['not found']
+        ]
+    ]);
+    $deletedAddress = App\Models\Address::where('id', $address->id)->first();
+    assertNotNull($deletedAddress);
 });
