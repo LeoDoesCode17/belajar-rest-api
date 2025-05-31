770dc551e5a6382466fda3210c2beaba2ccc8e2d
Leo <leonardonifinluri@gmail.com>
2025-05-31 11:51:37 +0800

    feat: add address seed in AddressSeeder

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,17 @@ class AddressSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::where('username', 'creator09')->first();
+        $contacts = $user->contacts->get();
+        foreach ($contacts as $key => $value) {
+            Address::create([
+                'street' => "Badak$key",
+                'city' => "Manado$key",
+                'province' => "Sulawesi Utara",
+                'country' => "Indonesia",
+                'postal_code' => "9023$key",
+                'contact_id' => $value->id
+            ]);
+        }
     }
 }
