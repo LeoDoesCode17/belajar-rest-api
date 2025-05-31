f00a57c68bf7578f464dda80fc57b7df5f0ff7bf
Leo <leonardonifinluri@gmail.com>
2025-05-31 13:28:12 +0800

    feat: add test cases for update an address failed and success

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -3,7 +3,11 @@
 use Database\Seeders\ContactSearchSeeder;
 use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\User;
+use App\Models\{User};
+use Database\Seeders\AddressSeeder;
+
+use function PHPUnit\Framework\assertNotEmpty;
+use function PHPUnit\Framework\assertNotEquals;
 
 uses(RefreshDatabase::class);
 
@@ -103,6 +107,118 @@ test('Failed to create an address due to one column exceeds characters length.',
         'province' => 'Sulawesi Utara',
         'country' => 'Indonesia',
         'postal_code' => '901249012490124901249012490124',
+    ])->assertStatus(400)->assertJson([
+        'errors' => [
+            'postal_code' => ['The postal code field must not be greater than 10 characters.']
+        ]
+    ]);
+});
+
+test('Succeed update an address.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $oldStreet = $address->street;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->put('/api/contacts/' . $contact->id . '/addresses/' . $address->id, [
+        'street' => 'Kebijaksanaan',
+        'city' => 'Manado',
+        'province' => 'Sulawesi Selatan',
+        'country' => 'Indonesia',
+        'postal_code' => '90124'
+    ])->assertStatus(200)->assertJson([
+        'data' => [
+            'street' => 'Kebijaksanaan',
+            'city' => 'Manado',
+            'province' => 'Sulawesi Selatan',
+            'country' => 'Indonesia',
+            'postal_code' => '90124',
+        ]
+    ]);
+    $newStreet = App\Models\Address::where('id', $address->id)->first()->street;
+    assertNotEquals($oldStreet, $newStreet);
+});
+
+test('Failed to update an address using other user contact.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user1 = User::where('username', 'creator09')->first();
+    $user2 = User::where('username', 'bowo09')->first();
+    $contact = $user1->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user2->token
+    ])->put('/api/contacts/' . $contact->id . '/addresses/' . $address->id, [
+        'street' => 'Kebijaksanaan',
+        'city' => 'Manado',
+        'province' => 'Sulawesi Selatan',
+        'country' => 'Indonesia',
+        'postal_code' => '90124'
+    ])->assertStatus(404)->assertJson([
+        'errors' => [
+            'message' => ['not found']
+        ]
+    ]);
+});
+
+test('Failed to update an address due to invalid/missing token.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $user->token = null;
+    $user->save();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->put('/api/contacts/' . $contact->id . '/addresses/' . $address->id, [
+        'street' => 'Kebijaksanaan',
+        'city' => 'Manado',
+        'province' => 'Sulawesi Selatan',
+        'country' => 'Indonesia',
+        'postal_code' => '90124'
+    ])->assertStatus(401)->assertJson([
+        'errors' => [
+            'message' => ['unauthorized']
+        ]
+    ]);
+});
+
+test('Failed to update an address due to not found address.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $oldStreet = $address->street;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->put('/api/contacts/' . $contact->id . '/addresses/' . ($address->id + 100), [
+        'street' => 'Kebijaksanaan',
+        'city' => 'Manado',
+        'province' => 'Sulawesi Selatan',
+        'country' => 'Indonesia',
+        'postal_code' => '90124'
+    ])->assertStatus(404)->assertJson([
+        'errors' => [
+            'message' => ['not found']
+        ]
+    ]);
+});
+
+test('Failed to update an address due to one column value exceeds length limit.', function () {
+    $this->seed([UserSeeder::class, ContactSearchSeeder::class, AddressSeeder::class]);
+    $user = User::where('username', 'creator09')->first();
+    $contact = $user->contacts->first();
+    $address = $contact->addresses->first();
+    $oldStreet = $address->street;
+    $this->withHeaders([
+        'Authorization' => $user->token
+    ])->put('/api/contacts/' . $contact->id . '/addresses/' . $address->id, [
+        'street' => 'Kebijaksanaan',
+        'city' => 'Manado',
+        'province' => 'Sulawesi Selatan',
+        'country' => 'Indonesia',
+        'postal_code' => '9012490124901249012490124901249012490124'
     ])->assertStatus(400)->assertJson([
         'errors' => [
             'postal_code' => ['The postal code field must not be greater than 10 characters.']
